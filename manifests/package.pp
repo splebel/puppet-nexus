37daa69736970c8c1d2a99f545f4f76d10d86038
Marc-André Courtois <mcourtois@assetscience.com>
2015-02-25 15:56:38 -0500

    Use full version for the download file, and ignore the revision if it's empty

--- manifests/package.pp
+++ manifests/package.pp
@@ -42,9 +42,14 @@ class nexus::package (
 
   $nexus_home      = "${nexus_root}/${nexus_home_dir}"
 
-  $full_version = "${version}-${revision}"
+  if $revision.empty? {
+    $full_version = $version
+  }
+  else {
+    $full_version = "${version}-${revision}"
+  }
 
-  $nexus_archive   = "nexus-${version}-bundle.tar.gz"
+  $nexus_archive   = "nexus-${full_version}-bundle.tar.gz"
   $download_url    = "${download_site}/${nexus_archive}"
   $dl_file         = "${nexus_root}/${nexus_archive}"
   $nexus_home_real = "${nexus_root}/nexus-${full_version}"
